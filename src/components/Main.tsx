97a3d9c05a49e67032f0710928d2b7e34dd73fcd
rkaufusi <rhettkaufusi@gmail.com>
2022-06-15 15:48:07 -0700

    changed img alt

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,7 +6,7 @@ const Main = () => {
         <div className="w-full h-auto my-10">
             <div className="grid md:grid-cols-2 m-auto">
                 <div className="flex justify-center">
-                    <img className="rounded-full mx-10" src="https://avatars.githubusercontent.com/u/29996077?v=4" alt="Rhett Image"/>
+                    <img className="rounded-full mx-10" src="https://avatars.githubusercontent.com/u/29996077?v=4" alt="Rhett"/>
                 </div>
                 <div className="flex flex-col items-center justify-center">
                     <h1 className="text-6xl">Rhett Kaufusi</h1>
